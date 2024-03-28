659cc5c7160775478f651ecd436bdb450a766702
Dome <arillota@gmail.com>
2024-03-28 11:24:37 +0100

    tareas

--- Practicas/32/script.js
+++ Practicas/32/script.js
@@ -7,3 +7,20 @@ function updateDateTime() {
 // Call the function initially and then set it to update every second
 updateDateTime();
 setInterval(updateDateTime, 1000);
+
+const imagen = document.querySelector(".cambio")
+console.log(imagen['src'])
+//No cambiar nada mas arriba
+
+//No cambiar esta line
+console.log(imagen.src)
+
+
+let images = [ "img/3.jpeg", "img/4.jpg", "img/5.jpg" ]
+
+function changeimg (img, imagepath){
+    img.src=imagepath;
+}
+setTimeout(() => {
+    changeimg(imagen,"img/8.jpeg" )
+}, 3000);
---
+++ Practicas/Pruebas3/script.js
@@ -0,0 +1,10 @@
+document.querySelector("p.select").style.backgroundColor = "red";
+
+const imagenes = ['i.jpg', '2.jpg', 
+'123']
+for (let i = 0; i < imagenes.length; i++) { 
+    const myh3 = document.createElement('img')
+    myh3.src = "Domenico booss" 
+    document.body.append(myh3)
+
+}
